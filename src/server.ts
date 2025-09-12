f6824eda1ffa051b2d82d8aefc68b2b2c901a813
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-09-12 20:12:18 +0600

    49-2 Setup Prisma in Starter Project

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,24 @@
 import http, { Server } from "http";
 import app from "./app";
 import dotenv from "dotenv";
+import { prisma } from "./config/db";
 
 dotenv.config();
 
 let server: Server | null = null;
 
+async function connectToDB() {
+  try {
+    await prisma.$connect()
+    console.log("Database Is Connected")
+  } catch (error) {
+    console.log("Database Db Connection Failed");
+    process.exit(1)
+  }
+}
 async function startServer() {
   try {
+    await connectToDB();
     server = http.createServer(app);
     server.listen(process.env.PORT, () => {
       console.log(`🚀 Server is running on port ${process.env.PORT}`);
