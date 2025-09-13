57570e65aacb956975575b9e297122fbd1475d54
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-09-13 21:22:58 +0600

    49-7 Get All Users

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -4,12 +4,22 @@ import { UserService } from "./user.service";
 const createUser = async (req:Request, res:Response) =>{
     try {
         const result = await UserService.createUser(req.body)
-        res.send(result)
+        res.status(201).json(result)
     } catch (error) {
         console.log(error)
+        res.status(500).send(error)
+    }
+} 
+const getAllUsersFromDB = async (req:Request, res:Response) =>{
+    try {
+        const result = await UserService.getAllUsersFromDB()
+        res.status(200).json(result)
+    } catch (error) {
+        res.status(500).send(error)
     }
 } 
 
 export const UserController = {
-    createUser
+    createUser,
+    getAllUsersFromDB
 }
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { UserController } from './user.controller';
 const router = express.Router();
 
+router.get("/", UserController.getAllUsersFromDB)
 router.post("/", UserController.createUser);
 
 export const UserRouter = router; 
